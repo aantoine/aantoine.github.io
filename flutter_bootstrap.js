78afa6d27ac3bddf926a6b5119dae7c312685899
Agustín Antoine <antoineagustin@gmail.com>
2026-08-20 17:23:31 -0400

    Built web
    Branch: main
    Commit: a0c1c86cafe8263c1eaa560bc15e39a4a88b2142 (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3110458609" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2435625047" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
